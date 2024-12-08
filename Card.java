bddf19b4abeb5c0324d8fb1f527f9c9311536eda
CallineKelly <167273812+CallineKelly@users.noreply.github.com>
2024-12-08 21:04:01 +0800

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,27 +1,35 @@
 package probability.project;
 
+import java.util.Objects;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Neoh
+ * get suit and rank from Deck class
+ */
 public class Card {
-    private final String suit;
-    private final String rank;
-
-    public Card(String suit, String rank) {
-        this.suit = suit.toLowerCase();
-        this.rank = rank.toLowerCase();
+    private String suit;
+    private String rank;
+    
+    public Card(String suit, String rank){
+        this.suit = suit;
+        this.rank = rank;
     }
-
-    public String getSuit() {
+    
+    public String getSuit(){
         return suit;
     }
-
-    public String getRank() {
+    
+    public String getRank(){
         return rank;
     }
-
-    @Override
-    public String toString() {
-        return rank + " of " + suit;
-    }
-
+    
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
@@ -29,13 +37,13 @@ public class Card {
         Card card = (Card) obj;
         return suit.equals(card.suit) && rank.equals(card.rank);
     }
-
+    
     @Override
     public int hashCode() {
-        return suit.hashCode() + rank.hashCode();
+        return Objects.hash(suit, rank);
     }
-}
-
+    
+    //help to return string like "Ace of Hearts", "2 of Hearts"
     @Override
     public String toString(){
         return rank + " of " +suit;
